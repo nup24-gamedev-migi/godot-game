3f53083521bb613ac6f2c83b3061ce1207edd091
InnocentusLime <innocentuslime.help@gmail.com>
2024-02-17 18:49:40 +0200

    Setup godot project

---
+++ src/test_script.gd
@@ -0,0 +1,12 @@
+class_name TestScript
+extends Node2D
+
+const ROT_SPEED: float = 2 * PI / 3
+
+func _ready():
+	print("test script is ready")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	rotate(ROT_SPEED * delta)
