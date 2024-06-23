824494d6b85bb0b62ca0237677123892669dbf0e
InnocentusLime <innocentuslime.help@gmail.com>
2024-06-23 13:49:54 +0300

    Tile walker API

--- src/logic/tile_walker.rs
+++ src/logic/tile_walker.rs
@@ -5,7 +5,10 @@
 //! direction (all it does is update the position if the
 //! direction points at a valid tile).
 
-use super::{World, Entity, Direction};
+use anyhow::Context;
+use macroquad::logging::error;
+
+use super::{World, Entity, Direction, tile};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct TileWalkerPos(pub Entity);
@@ -13,10 +16,27 @@ pub struct TileWalkerPos(pub Entity);
 #[derive(Clone, Copy, Debug)]
 pub struct TileWalkerMovement(pub Option<Direction>);
 
-pub fn get_future_walker_tile(world: &mut World, walker: Entity) -> Option<Entity> {
-    todo!()
+pub fn get_future_walker_tile(world: &World, walker: Entity) -> anyhow::Result<Option<Entity>> {
+    let mut query = world.query_one::<(&TileWalkerPos, &TileWalkerMovement)>(walker)
+                     .context("Accessing entity data")?;
+    let (pos, dir) = query.get().unwrap();
+    let Some(dir) = dir.0 else { return Ok(None); };
+    let pos = pos.0;
+
+    tile::get_tile_neighbor(world, pos, dir)
+        .ok_or_else(|| anyhow::anyhow!("Walker is targetting a non-existent tile"))
+        .map(Some)
 }
 
-pub fn update_walkers(world: &mut World) {
-    todo!()
+pub fn update_walkers(world: &World) {
+    for (e, (pos, dir)) in world.query::<(&mut TileWalkerPos, &mut TileWalkerMovement)>().iter() {
+        let Ok (new_pos) = get_future_walker_tile(world, e) else {
+            error!("Entity {:?} failed to move in dir {:?}", e, dir);
+            continue;
+        };
+        let Some(new_pos) = new_pos else { continue; };
+
+        pos.0 = new_pos;
+        dir.0 = None;
+    }
 }
